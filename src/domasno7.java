fdd60debf931b41f9bcf16cfc0232ba6201ee3b3
Verica100ilkova <verica.stoilkova@students.finki.ukim.mk>
2023-04-08 15:55:29 +0200

    New branch

---
+++ src/domasno7.java
@@ -0,0 +1,12 @@
+import org.apache.jena.vocabulary.RDF;
+
+import static org.apache.jena.vocabulary.SchemaDO.model;
+
+public class domasno7 {
+
+    String personURI = "https://www.linkedin.com/in/verica-stoilkova-482722255/";
+    String fullName = "Verica Stoilkova";
+    String birthDate = "10.09.2000";
+    String givenName = "Verica";
+    String familyName = "Stoilkova";
+}
